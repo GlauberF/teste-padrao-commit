25f980759f9bc5179d55aa7ccf8c1b90753716b3
Glauber Funez <glauber.funez@asaas.com.br>
2024-01-30 09:35:14 -0300

    fix: improvements

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # teste-padrao-commit
 
 Passos Instalação
-- npm i commitizen @digitalroute/cz-conventional-changelog-for-jira
+- npm i commitizen @glauberfunez/cz-conventional-changelog-for-jira/types
 - npm i simple-git-hooks --save-dev
 - npm i prettier --save-dev
 - npm i lint-staged --save-dev
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('teste');
+// const custom = require('@glauberfunez/cz-conventional-changelog-for-jira/configurable');
+//const defaultTypes = require('@glauberfunez/cz-conventional-changelog-for-jira/types');
+
+module.exports = custom({
+  // types: {
+  //   ...defaultTypes,
+  //   perf: {
+  //     description: 'algo',
+  //     title: 'algo'
+  //   }
+  // },
+  // skipType: true,
+  // skipScope: true,
+});
